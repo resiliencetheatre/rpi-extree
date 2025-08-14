ccd1c94158b41df203acdc300dcc2382f150b435
info <info@resilience-theatre.com>
2025-08-14 07:43:42 +0200

    * Added macpipe.service to usb-plug.target * Updated lvgl-com version

--- package/lvgl-com/lvgl-com.mk
+++ package/lvgl-com/lvgl-com.mk
@@ -1,4 +1,4 @@
-LVGL_COM_VERSION = e68b537cdd8acd513f0f1b511fe11f54956ea8af
+LVGL_COM_VERSION = 09005db69ac81bcb6205875718a535864443cc7b
 LVGL_COM_SITE = https://codeberg.org/resiliencetheatre/lvgl-com.git
 LVGL_COM_SITE_METHOD = git
 LVGL_COM_DEPENDENCIES += libpng zlib
