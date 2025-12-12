8cf420dd4dea93e1033843d79dbd4531a8a90001
info <info@resilience-theatre.com>
2025-12-12 08:28:16 +0100

    * corrected install cmd on edgemap package

--- package/edgemap/edgemap.mk
+++ package/edgemap/edgemap.mk
@@ -5,15 +5,15 @@
 ################################################################################
 
 EDGEMAP_SITE = https://codeberg.org/resiliencetheatre/edgeui.git
-EDGEMAP_VERSION = b743efb31469708df8a22f9b6b043ebe3d3ed90d
+EDGEMAP_VERSION = 8707f7939a2d58c452837bff3f7bd8a605626cb4
 EDGEMAP_SITE_METHOD = git
 
 EDGEMAP_LICENSE = Custom
 EDGEMAP_LICENSE_FILES =
 
 define EDGEMAP_INSTALL_TARGET_CMDS
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/opt/edgemap
-	cp -a $(@D)/* $(TARGET_DIR)/opt/edgemap/
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/opt/edgemap/edgeui
+	cp -r -a $(@D)/* $(TARGET_DIR)/opt/edgemap/edgeui
 endef
 
 $(eval $(generic-package))
