8b3cf9aceb84441a50a8e4593503bb93689478f9
info <info@resilience-theatre.com>
2026-03-20 06:17:04 +0100

    * ptt_helper to fire PTT button from lvgl code (via audiostreamer-udpptt.service) * audioreceiver-udpptt.service fires up ptt_client * for some reason, usb headset gives failure for first audio access after which it works   to overcome this, we play dummy sound at boot with initialplay.service

--- package/udpptt/udpptt.mk
+++ package/udpptt/udpptt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UDPPTT_VERSION = 5beff4f114afb6c530de47b3e3d173af75f15fd9
+UDPPTT_VERSION = 9de9317ae39cb1fca954ee139a2f368d953befee
 UDPPTT_SITE = https://codeberg.org/resiliencetheatre/udpptt.git
 UDPPTT_SITE_METHOD = git
 UDPPTT_LICENSE = GPL-3.0-or-later
@@ -18,10 +18,11 @@ UDPPTT_BUILD_CMDS = \
 		CFLAGS="$(TARGET_CFLAGS)" \
 		LDFLAGS="$(TARGET_LDFLAGS)" \
 		PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
-		ptt_client ptt_server
+		ptt_client ptt_server ptt_helper
 
 UDPPTT_INSTALL_TARGET_CMDS = \
 	$(INSTALL) -D -m 0755 $(@D)/ptt_client $(TARGET_DIR)/usr/bin/ptt_client && \
-	$(INSTALL) -D -m 0755 $(@D)/ptt_server $(TARGET_DIR)/usr/bin/ptt_server
+	$(INSTALL) -D -m 0755 $(@D)/ptt_server $(TARGET_DIR)/usr/bin/ptt_server && \
+	$(INSTALL) -D -m 0755 $(@D)/ptt_helper $(TARGET_DIR)/usr/bin/ptt_helper
 
 $(eval $(generic-package))
