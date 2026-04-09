64b3b1f722894e2d03863b14b7ebeb27aac37faf
info <info@resilience-theatre.com>
2026-04-09 06:22:07 +0200

    * knock-daemon version upgrade

---
+++ package/knock-daemon/knock-daemon.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# knock-daemon
+#
+################################################################################
+
+KNOCK_DAEMON_VERSION = fdfd5bc5821c644932aa8cfbe0b9f56711a35b8d
+KNOCK_DAEMON_SITE = https://codeberg.org/resiliencetheatre/knock-daemon.git
+KNOCK_DAEMON_SITE_METHOD = git
+
+define KNOCK_DAEMON_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS)"
+endef
+
+define KNOCK_DAEMON_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/knock-send \
+		$(TARGET_DIR)/usr/bin/knock-send
+	$(INSTALL) -D -m 0755 $(@D)/knock-receiver \
+		$(TARGET_DIR)/usr/bin/knock-receiver
+        $(INSTALL) -D -m 0755 $(@D)/knock_cmd_receive \
+                $(TARGET_DIR)/usr/bin/knock_cmd_receive
+        $(INSTALL) -D -m 0755 $(@D)/knock_cmd_send \
+                $(TARGET_DIR)/usr/bin/knock_cmd_send
+endef
+
+$(eval $(generic-package))
