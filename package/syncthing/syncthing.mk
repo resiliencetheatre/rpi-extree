4a808a56bfda80232c04ba2b86bdbb67a83a7ba9
info <info@resilience-theatre.com>
2026-05-10 02:24:03 +0200

    * syncthing v2.0.16 for edgemap

--- package/syncthing/syncthing.mk
+++ package/syncthing/syncthing.mk
@@ -7,24 +7,37 @@ SYNCTHING_SITE          = $(call github,syncthing,syncthing,$(SYNCTHING_VERSION)
 SYNCTHING_LICENSE       = MPLv2
 SYNCTHING_LICENSE_FILES = LICENSE
 
-# Ensure the proper host Go is available first.
-SYNCTHING_DEPENDENCIES += host-go
+SYNCTHING_GOMOD = github.com/syncthing/syncthing
 
-# Target build via golang-package
-SYNCTHING_GO_ENV        = GO111MODULE=on CGO_ENABLED=0 GOTOOLCHAIN=local
+SYNCTHING_DEPENDENCIES += host-go sqlite
+
+SYNCTHING_DL_ENV += \
+        GOPROXY=https://proxy.golang.org,direct \
+        GOSUMDB=sum.golang.org \
+        GOTOOLCHAIN=local
+
+SYNCTHING_GO_ENV += \
+        GO111MODULE=on \
+        CGO_ENABLED=1 \
+        GOPROXY=https://proxy.golang.org,direct \
+        GOSUMDB=sum.golang.org \
+        GOTOOLCHAIN=local
+
 SYNCTHING_BUILD_TARGETS = ./cmd/syncthing
 
-# Generate embedded assets (defines auto.Assets) using host Go before target build.
 SYNCTHING_PRE_BUILD_HOOKS += SYNCTHING_GEN_ASSETS
 define SYNCTHING_GEN_ASSETS
-	cd $(@D) && { \
-		export PATH="$(HOST_DIR)/bin:$(HOST_GO_ROOT)/bin:$(BR_PATH)"; \
-		export GOROOT="$(HOST_GO_ROOT)"; \
-		export GOPATH="$(HOST_GO_GOPATH)"; \
-		export GO111MODULE=on GOTOOLCHAIN=local; \
-		echo "using go: $$(which go)"; go version; \
-		go run build.go assets; \
-	}
+        cd $(@D) && { \
+                export PATH="$(HOST_DIR)/bin:$(HOST_GO_ROOT)/bin:$(BR_PATH)"; \
+                export GOROOT="$(HOST_GO_ROOT)"; \
+                export GOPATH="$(HOST_GO_GOPATH)"; \
+                export GO111MODULE=on; \
+                export GOTOOLCHAIN=local; \
+                export GOPROXY=https://proxy.golang.org,direct; \
+                export GOSUMDB=sum.golang.org; \
+                echo "using go: $$(which go)"; go version; \
+                go run build.go assets; \
+        }
 endef
 
 $(eval $(golang-package))
