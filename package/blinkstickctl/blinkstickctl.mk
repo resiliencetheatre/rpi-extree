a5f36cc2d2188dc5c0975668a4c50f47d8984158
info <info@resilience-theatre.com>
2026-05-15 06:17:37 +0200

    * New package blinkstickctl

---
+++ package/blinkstickctl/blinkstickctl.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# blinkstickctl
+#
+################################################################################
+
+# For reproducible Buildroot builds, replace "main" with a fixed commit hash
+# or a release tag once you decide which repository revision to pin.
+BLINKSTICKCTL_VERSION = 5fed6e0428f82f51614a523a6b731e3453bdc65a
+BLINKSTICKCTL_SITE = https://codeberg.org/resiliencetheatre/blinkstickctl.git
+BLINKSTICKCTL_SITE_METHOD = git
+
+BLINKSTICKCTL_DEPENDENCIES = libusb
+
+define BLINKSTICKCTL_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		-C $(@D) \
+		blinkstickctl \
+		LIBUSB_CFLAGS="-I$(STAGING_DIR)/usr/include/libusb-1.0" \
+		LIBUSB_LIBS="-lusb-1.0"
+endef
+
+define BLINKSTICKCTL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/blinkstickctl \
+		$(TARGET_DIR)/usr/bin/blinkstickctl
+endef
+
+$(eval $(generic-package))
