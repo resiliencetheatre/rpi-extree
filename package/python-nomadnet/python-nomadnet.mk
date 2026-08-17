aae0e0c2034801b4e1c52274740782e2b0c23d8a
info <info@resilience-theatre.com>
2026-08-17 03:39:43 +0200

    * exim, dovecot added and rns, nomadnet updated

--- package/python-nomadnet/python-nomadnet.mk
+++ package/python-nomadnet/python-nomadnet.mk
@@ -2,18 +2,19 @@
 #
 # python-nomadnet
 #
-# https://pypi.org/project/nomadnet/
-# https://files.pythonhosted.org/packages/4c/1f/52e2cb9510108583a74ff8b46c77650ce4e14b811759f93f82e2b3583e6b/nomadnet-0.4.9.tar.gz
-#
 ################################################################################
 
-PYTHON_NOMADNET_VERSION = 0.4.9
-PYTHON_NOMADNET_SOURCE = nomadnet-$(PYTHON_NOMADNET_VERSION).tar.gz
-PYTHON_NOMADNET_SITE = https://files.pythonhosted.org/packages/4c/1f/52e2cb9510108583a74ff8b46c77650ce4e14b811759f93f82e2b3583e6b
-PYTHON_NOMADNET_LICENSE = MIT
-PYTHON_NOMADNET_LICENSE_FILES = LICENSE-PSF LICENSE
+PYTHON_NOMADNET_VERSION = 1.2.0
+PYTHON_NOMADNET_SITE = $(call github,markqvist,NomadNet,$(PYTHON_NOMADNET_VERSION))
 PYTHON_NOMADNET_SETUP_TYPE = setuptools
-# This is a runtime dependency, but we don't have the concept of
-# runtime dependencies for host packages.
+
+PYTHON_NOMADNET_LICENSE = GPL-3.0
+PYTHON_NOMADNET_LICENSE_FILES = LICENSE
+
+PYTHON_NOMADNET_DEPENDENCIES = \
+	python-rns \
+	python-lxmf \
+	python-urwid \
+	python-qrcode
 
 $(eval $(python-package))
