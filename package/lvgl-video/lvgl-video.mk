480126f78ea9045e800e2b64e81b8def50b977fa
info <info@resilience-theatre.com>
2026-09-20 07:13:36 +0200

    raspberrypi4_videoterm_defconfig
     * lvgl-video interoperability with gtk-pipe
       - enabled echo cancel

--- package/lvgl-video/lvgl-video.mk
+++ package/lvgl-video/lvgl-video.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LVGL_VIDEO_VERSION = bbbc1c53559ac45fe5fc85058170c16bfcc5ba8a
+LVGL_VIDEO_VERSION = 7a9f7257fd6fb7c1babc4401d4e23ca6524c65dc
 LVGL_VIDEO_SITE = https://github.com/resiliencetheatre/lvgl-video.git
 LVGL_VIDEO_SITE_METHOD = git
-LVGL_VIDEO_DEPENDENCIES = host-pkgconf gstreamer1 gst1-plugins-base libcamera
+LVGL_VIDEO_DEPENDENCIES = host-pkgconf gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad libcamera
 LVGL_VIDEO_LICENSE = MIT
 LVGL_VIDEO_LICENSE_FILES = LICENSE third_party/lvgl/LICENCE.txt
 
